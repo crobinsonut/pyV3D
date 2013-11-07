62038fe866fe11c30d570a70db11792a093a4a4c
Sven <pyvelepor@gmail.com>
2013-11-07 10:34:57 -0500

    Copied over find_dll from mingw32ccompiler

--- setup.py
+++ setup.py
@@ -11,6 +11,31 @@ except ImportError:
     print 'numpy was not found.  Aborting build'
     sys.exit(-1)
 
+def find_dll(dll_name):
+
+    arch = {'AMD64' : 'amd64',
+            'Intel' : 'x86'}[get_build_architecture()]
+
+    def _find_dll_in_winsxs(dll_name):
+        # Walk through the WinSxS directory to find the dll.
+        winsxs_path = os.path.join(os.environ['WINDIR'], 'winsxs')
+        if not os.path.exists(winsxs_path):
+            return None
+        for root, dirs, files in os.walk(winsxs_path):
+            if dll_name in files and arch in root:
+                return os.path.join(root, dll_name)
+        return None
+
+    def _find_dll_in_path(dll_name):
+        # First, look in the Python directory, then scan PATH for
+        # the given dll name.
+        for path in [sys.prefix] + os.environ['PATH'].split(';'):
+            filepath = os.path.join(path, dll_name)
+            if os.path.exists(filepath):
+                return os.path.abspath(filepath)
+
+    return _find_dll_in_winsxs(dll_name) or _find_dll_in_path(dll_name)
+
 def find_libgcc_dll(dll_name):
     gcc_exe_file = find_gcc_exe()
     gcc_bin_dir = gcc_exe_file.rstrip("gcc.exe")
